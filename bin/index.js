256c0560a615fb587ba8b2c09e63762cdd0df192
ws <wshappyday@sina.com>
2023-05-21 23:29:50 +0800

    关联wscfan-cli-lib

--- bin/index.js
+++ bin/index.js
@@ -1,3 +1,31 @@
 #!/usr/bin/env node
 
-console.log('welcome imooc-test')
+const lib = require("wscfan-cli-lib");
+
+// 注册一个命令 wscfan-cli init
+const argv = require("process").argv;
+const command = argv[2];
+
+const options = argv.slice(3);
+if (options.length > 1) {
+  let [option, param] = options;
+  option = option.replace("--", "");
+
+  if (command) {
+    if (lib[command]) {
+      lib[command]({ option, param });
+    } else {
+      console.log("请输入命令");
+    }
+  } else {
+    console.log("无效命令");
+  }
+}
+
+// 实现参数解析 --version 和 init --name
+if (command.startsWith("--") || command.startsWith("-")) {
+  const globalOption = command.replace(/--|-/g, "");
+  if (globalOption === "version" || globalOption === "V") {
+    console.log("1.0.3");
+  }
+}
